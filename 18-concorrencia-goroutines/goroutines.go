98e3fc2c53d08effce52acf3a72d76b782483e40
Maurício dos Santos <mauriciodssantosds@gmail.com>
2024-11-12 11:35:29 -0300

    go routines

---
+++ 18-concorrencia-goroutines/goroutines.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go escrever("Olá mundo!") //goroutine não espera acabar para seguir para a próxima linha
+	escrever("Programando em Go!") // se colocar go em tudo ele não vai imprimir nada pq não espera executar e termina o programa
+}
+
+func escrever(texto string) {
+	for {
+		fmt.Println(texto)
+		time.Sleep(time.Second)
+	}
+}
